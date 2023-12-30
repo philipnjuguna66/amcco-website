a7c157df2f451f0e1bc0535c628f8d858f78c159
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-30 15:14:38 +0300

    initial commit

--- resources/views/livewire/blog/blog-list.blade.php
+++ resources/views/livewire/blog/blog-list.blade.php
@@ -17,7 +17,7 @@
                             </a>
                         </h3>
                         <p class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">
-                            {{ str( $blog->meta_description)->toHtmlString()  }}
+                            {{ str( $blog->meta_description)->stripTags('p,span')->toHtmlString()  }}
                         </p>
                     </div>
                 </div>
