7079bb54386b86ea5fc94f1185473e811a614a61
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-30 15:30:10 +0300

    article page

--- resources/views/pages/post/single.blade.php
+++ resources/views/pages/post/single.blade.php
@@ -11,34 +11,7 @@
     @endpush
 
 
-    <section class="mt-28 py-4 md:mx-auto max-w-7xl md:w-4/5 px-8 prose-md ">
-        <div class="mt-4">
-            <div class="bg-transparent px-6 pt-4 sm:pt-12 lg:px-8 flex justify-center max-w-2xl">
-                <h1 class="font-bold text-2xl text-center">{{ $post->title }}</h1>
-            </div>
-        </div>
 
-        <div class="md:mx-auto md:w-4/5 md:max-w-4xl ">
-            <div class="col-span-2 ">
-                <img src="{{ \Illuminate\Support\Facades\Storage::url($post?->featured_image) }}"
-                     class="w-[800px] h-[500px] rounded-md object-contain aspect-ratio"
-                     alt="{{ $post?->meta_title }}"
-                     loading="lazy"
-                >
-                <article class="prose mt-12 max-w-7xl justify-center align-middle">
-
-                    {{ str($post?->body)->toHtmlString() }}
-
-                </article>
-            </div>
-            <div class="col-span-1">
-
-
-
-            </div>
-        </div>
-
-    </section>
 
 
 </x-guest-layout>
