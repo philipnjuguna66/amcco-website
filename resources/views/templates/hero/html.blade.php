f0bd0cc1ce7ae775269bdbc847afd50695e47bca
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-30 21:19:48 +0300

    text area widhet

--- resources/views/templates/hero/html.blade.php
+++ resources/views/templates/hero/html.blade.php
@@ -1,5 +1,5 @@
 
-<div class="bg-gray-50 md:py-12 ">
+<div class="bg-white md:py-12 ">
     <div class="md:mx-auto md:w-4/5 max-w-7xl px-2 lg:px-8">
 {{ str($section->extra['html'])->toHtmlString() }}
     </div>
