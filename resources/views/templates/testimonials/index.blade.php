99844d18c0df36fc3a143ead59f6bb3c6270c3cc
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-30 22:56:58 +0300

    text area widhet

--- resources/views/templates/testimonials/index.blade.php
+++ resources/views/templates/testimonials/index.blade.php
@@ -50,7 +50,7 @@
                                     href="{{ $review['author_url'] }}"
                                     class="text-sm text-secondary-600"
                                     target="_blank">
-                                    Read More
+                                    Read More Reviews
                                 </a>
                             @endif
                         </p>
