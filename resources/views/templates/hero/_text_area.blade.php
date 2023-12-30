e2d94bfd8108dad5d62ec4469fc244c272812a25
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-30 20:42:47 +0300

    text area widhet

--- resources/views/templates/hero/_text_area.blade.php
+++ resources/views/templates/hero/_text_area.blade.php
@@ -1,6 +1,6 @@
 <div class="@if($hasBorderColor) shadow-md rounded-md px-4 mt-5 bg-gray-100 py-8 border-b-4 border-primary-600 border-b-primary-600 @endif">
 
-    <div class="prose  md:text-justify py-4 leading-6 tracking-loose">
+    <div class="prose py-4 leading-6 tracking-loose">
         {{ str($html)->trim(' ')->toHtmlString() }}
     </div>
 
