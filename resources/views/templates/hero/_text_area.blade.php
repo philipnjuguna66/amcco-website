40728bbfa2880709d0f97397a19ecd3c58d2a193
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-30 21:19:37 +0300

    text area widhet

--- resources/views/templates/hero/_text_area.blade.php
+++ resources/views/templates/hero/_text_area.blade.php
@@ -1,4 +1,4 @@
-<div class="@if($hasBorderColor) shadow-md rounded-md px-4 mt-5 bg-gray-100 py-8 border-b-4 border-primary-600 border-b-primary-600 @endif">
+<div class="@if($hasBorderColor) shadow-md rounded-md px-4  g-gray-100 py-8 border-b-4 border-primary-600 border-b-primary-600 @endif">
 
     <div class="prose py-4 leading-6 tracking-loose">
         {{ str($html)->trim(' ')->toHtmlString() }}
--- resources/views/templates/hero/html.blade.php
+++ resources/views/templates/hero/html.blade.php
@@ -1,3 +1,8 @@
- {{ str($section->extra['html'])->toHtmlString() }}
+
+<div class="bg-gray-50 md:py-12 ">
+    <div class="md:mx-auto md:w-4/5 max-w-7xl px-2 lg:px-8">
+{{ str($section->extra['html'])->toHtmlString() }}
+    </div>
+</div>
 
 
