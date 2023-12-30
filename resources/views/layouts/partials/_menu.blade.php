35a1b2d6f82268fb70b4e313b736d4aa59ee279d
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-30 22:05:21 +0300

    text area widhet

--- resources/views/layouts/partials/_menu.blade.php
+++ resources/views/layouts/partials/_menu.blade.php
@@ -8,7 +8,7 @@ $menu = FilamentNavigation::get('header');
 
             <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li class="py-2 border-b-1 border-primary-300/50 md:border-b-0">
-                    <a wire:navigate href="{{ url('/') }}" class="block  py-1 text-center  {{ activeLink("/")  ? "border-b-2 border-primary-900 text-gray-900 font-bold " : "    text-gray-800  " }} rounded md:bg-transparent dark:bg-blue-600 md:dark:bg-transparent"  @if(activeLink("/") ) aria-current="page" @endif>{{ str("HOME")->title()->toString() }}</a>
+                    <a wire:navigate href="{{ url('/') }}" class="block  py-1 text-center  {{ activeLink("/")  ? "border-b-2 border-primary-900 text-secondary-600 font-bold" : "    text-gray-800  " }} rounded md:bg-transparent dark:bg-blue-600 md:dark:bg-transparent"  @if(activeLink("/") ) aria-current="page" @endif>{{ str("HOME")->title()->toString() }}</a>
                 </li>
 
 
@@ -36,7 +36,7 @@ $menu = FilamentNavigation::get('header');
 
                         id="{{ str($item['label'])->title()->slug()->append('Link')->toString()  }}"
                             data-dropdown-toggle="{{ str($item['label'])->title()->slug()->toString()  }}"
-                            class="{{activePrentLink($item['children'])  ? "border-b-2 border-primary-900 text-gray-900  font-bold py-2" : "    text-gray-800  md:border-0  " }} flex items-center text-center justify-center md:justify-between w-full mt-1 py-1  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-primary-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
+                            class="{{activePrentLink($item['children'])  ? "border-b-2 border-primary-900 text-secondary-600 font-bold py-2" : "    text-gray-800  md:border-0  " }} flex items-center text-center justify-center md:justify-between w-full mt-1 py-1  rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-primary-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
                         {{ str($item['label'])->toString()  }}
                         <svg class="w-2.5 h-2.5 ml-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                             <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
