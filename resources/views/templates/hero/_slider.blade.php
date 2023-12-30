ebc2e8fb4f061240bc70fb2765a2773f0896c5f4
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-30 10:56:00 +0300

    default colors

--- resources/views/templates/hero/_slider.blade.php
+++ resources/views/templates/hero/_slider.blade.php
@@ -1,7 +1,7 @@
 <div class="bg-white relative">
-    <div class="w-auto" data-carousel="slide">
+    <div class="w-4/5" data-carousel="slide">
 
-        <div class="relative h-36 overflow-hidden lg:h-96">
+        <div class="relative h-56 overflow-hidden lg:h-96">
             @foreach($sliders as $image)
 
                 <div class=" duration-700  ease-in-out" data-carousel-item>
