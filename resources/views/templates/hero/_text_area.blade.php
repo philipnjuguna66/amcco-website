df6d729724397c49a7e435a6da9811c0d501b112
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-30 22:35:59 +0300

    text area widhet

--- resources/views/templates/hero/_text_area.blade.php
+++ resources/views/templates/hero/_text_area.blade.php
@@ -1,6 +1,6 @@
 <div class="@if($hasBorderColor) shadow-md rounded-md px-4  w-full g-gray-100 py-8 border-b-4 border-primary-600 border-b-primary-600 @endif">
 
-    <div class="prose">
+    <div class="">
         {{ str($html)->trim(' ')->toHtmlString() }}
     </div>
 
--- resources/views/templates/hero/page_builder.blade.php
+++ resources/views/templates/hero/page_builder.blade.php
@@ -29,7 +29,7 @@
                                     "default" => null,
                                 };
                                 ?>
-                            <div class="md:max-w-7xl mx-auto md:px-2 prose">
+                            <div class="md:max-w-7xl mx-auto md:px-2">
 
 
                                 {{ str($html)->toHtmlString() }}
