d135a0f0ea7910632182dbf7f2c2546cbef10424
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-30 22:33:45 +0300

    text area widhet

--- resources/views/templates/hero/page_builder.blade.php
+++ resources/views/templates/hero/page_builder.blade.php
@@ -15,7 +15,7 @@
         <div class="mt-4 py-4">
             <div class="grid grid-cols-1 md:grid-cols-{{ $section->extra['columns'] }}  gap-x-2 gap-y-2">
                 @foreach($section->extra['columns_sections'] as $index => $columns)
-                    <div class="">
+                    <div class=" ">
                         @foreach($columns as $column)
                                 <?php
                                 $html = match ($column['type']) {
@@ -23,14 +23,17 @@
                                     "video" => view('templates.embeded._video_iframe', ["autoplay" => 0, 'videoUri' => $column['data']['video_path']])->render(),
                                     "image" => view('templates.hero._image', ['image' => $column['data']['image'], "title" => $column['data']['title'], 'section' => $section])->render(),
                                     "booking_form" => view('templates.hero._site')->render(),
-                                    "text_area" => view('templates.hero._text_area', ['html' => $column['data']['body'],'hasBorderColor' => $column['data']['has_border_bottom'] ?? false])->render(),
+                                    "text_area" => view('templates.hero._text_area', ['html' => $column['data']['body'], 'hasBorderColor' => $column['data']['has_border_bottom'] ?? false])->render(),
                                     "sliders" => view('templates.hero._slider', ['sliders' => $column['data']['images'], 'page' => $page])->render(),
                                     "masonary_block" => view('templates.hero.masionary', ['masonrySections' => $column['data']['masonary_block'], 'page' => $page])->render(),
                                     "default" => null,
                                 };
                                 ?>
+                            <div class="md:max-w-7xl mx-auto md:px-2 prose">
+
 
                                 {{ str($html)->toHtmlString() }}
+                            </div>
 
                         @endforeach
                     </div>
