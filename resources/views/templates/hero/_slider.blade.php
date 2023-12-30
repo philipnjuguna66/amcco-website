e4f87bb6b875d2c903515e4a6baf18cbae009ea4
Njuguna Philip <philipnjuguna66@gmail.com>
2023-12-30 10:53:43 +0300

    default colors

--- resources/views/templates/hero/_slider.blade.php
+++ resources/views/templates/hero/_slider.blade.php
@@ -1,12 +1,12 @@
 <div class="bg-white relative">
-    <div class="w-full" data-carousel="slide">
+    <div class="w-auto" data-carousel="slide">
 
         <div class="relative h-36 overflow-hidden lg:h-96">
             @foreach($sliders as $image)
 
                 <div class=" duration-700  ease-in-out" data-carousel-item>
                     <img src="{{ \Illuminate\Support\Facades\Storage::url($image) }}"
-                         class="absolute block object-cover bg-center bg-contain bg-no-repeat  w-full object-center -translate-x-1/2 -translate-y-1/2  top-1/2 left-1/2"
+                         class="absolute block object-cover  -translate-x-1/2 -translate-y-1/2  top-1/2 left-1/2"
                          alt="{{ $page->meta_title }}">
                 </div>
 
