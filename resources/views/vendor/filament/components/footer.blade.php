0e5db07c1741f562412f4d02f331c6837a94fd41
Njuguna Philip <philipnjuguna66@gmail.com>
2024-01-17 11:10:10 +0300

    text area widhet

--- resources/views/vendor/filament/components/footer.blade.php
+++ resources/views/vendor/filament/components/footer.blade.php
@@ -3,7 +3,7 @@
 <div class="filament-footer flex items-center justify-center">
     {{ \Filament\Facades\Filament::renderHook('footer.start') }}
 
-   <h3 class="font-extralight text-gray-300">InsightPro v3.0.0 | All Rights Reserved: &copy; {{ now()->format('Y') }}</h3>
+   <h3 class="font-extralight text-gray-300"> All Rights Reserved: &copy; {{ now()->format('Y') }}</h3>
 
 
     {{ \Filament\Facades\Filament::renderHook('footer.end') }}
