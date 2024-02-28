aed1a532f4dbfa8a9c45f6927118994ff108fcb2
Njuguna Philip <philipnjuguna66@gmail.com>
2024-02-28 17:55:51 +0300

    text area widhet

--- resources/views/templates/hero/links.blade.php
+++ resources/views/templates/hero/links.blade.php
@@ -1,7 +1,7 @@
 <div class=" ">
-    <div class="px-6 py-2 sm:px-6 sm:py-1 lg:px-8">
+    <div class="">
         <div class="md:mx-auto max-w-2xl text-center">
-            <div class="mt-10 flex items-center justify-center gap-x-6">
+            <div class="flex items-center justify-center gap-x-6">
                 <a
                     href="{{ route('permalink.show', $link['url']) }}"
                     class="button bg-secondary-600 hover:bg-primary-600 text-white no-underline	hover:ease-in-out duration-300 transition ">
