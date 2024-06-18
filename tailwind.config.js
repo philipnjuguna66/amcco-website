e830e15454af37adfabb98024032a9ef5e2268ee
philipnjuguna66 <philipnjuguna66@gmail.com>
2024-06-18 10:44:36 +0300

    bg white

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
                 poppins: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
             colors: {
+                white: "rgb(255, 255, 255)",
                 primary: {
                     950 :  "#145359",
                     900 :  "#145359",
