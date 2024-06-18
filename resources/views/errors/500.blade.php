ef64512e4ef1e75739308f163844bdb0d7122723
philipnjuguna66 <philipnjuguna66@gmail.com>
2024-06-18 17:21:55 +0300

    bg white

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,8 +1,15 @@
 <x-guest-layout>
 @section('title',"Something went wrong")
 @section('description', "500")
-    @if(str(url()->current())->contains(config('filament.path')))
-        {{ str($exception->getMessage() ?? " ")->toHtmlString() }}
-    @endif
-    <h2 class="text-2xl font-extrabold">Something not right~ ! </h2>
+   <div class="mx-auto py-3 mt-12 max-w-4xl">
+       @if(str(url()->current())->contains(config('filament.path')))
+           <div class="text-2xl font-extrabold">
+               {{ str($exception->getMessage() ?? " ")->toHtmlString() }}
+           </div>
+
+       @else
+           <h2 class="text-2xl font-extrabold">Something not right~ ! </h2>
+       @endif
+   </div>
+
 </x-guest-layout>
