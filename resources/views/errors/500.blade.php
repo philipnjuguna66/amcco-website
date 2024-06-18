be8762fc5e68545ce6b9462661393eac7cef9698
philipnjuguna66 <philipnjuguna66@gmail.com>
2024-06-18 17:20:05 +0300

    bg white

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,5 +1,8 @@
-@extends('errors::minimal')
-
-@section('title', __('Server Error'))
-@section('code', '500')
-@section('message', __('Server Error'))
+<x-guest-layout>
+@section('title',"Something went wrong")
+@section('description', "500")
+    @if(str(url()->current())->contains(config('filament.path')))
+        {{ str($exception->getMessage() ?? " ")->toHtmlString() }}
+    @endif
+    <h2 class="text-2xl font-extrabold">Something not right~ ! </h2>
+</x-guest-layout>
